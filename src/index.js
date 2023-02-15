e3d8404d8cdbc94ae8a7456ae85a769062d57f51
rituraj00 <rituraj171926@gmail.com>
2023-02-15 22:30:53 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient("https://ywiepbwlvqdctutfvila.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3aWVwYndsdnFkY3R1dGZ2aWxhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzY0NzkwOTAsImV4cCI6MTk5MjA1NTA5MH0.7U-E-YAUeKVJ5qiD5MhKxVykIHlDQaVWQqP_hYKNVBU")
+
+async function getCountries(){
+  const countries = await supabase.from('countries').select()
+
+  console.log(countries);
+}
+
+getCountries()
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
